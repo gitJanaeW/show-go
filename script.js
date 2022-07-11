037da8ec228f9cc9dc1592a53712f1c64d3ed843
janae.wel <janae.wel@gmail.com>
2022-07-10 21:32:07 -0400

    generate tickets in DOM

--- script.js
+++ script.js
@@ -149,14 +149,46 @@ function getConcertValues(concerts){
 function createResults(){
     for(let i = 0; i < 5; i++){
         // create DOm elements
+        var concertCard = document.createElement("div");
+        concertCard.classList = "card";
+
+        var cardContent = document.createElement("div");
+        cardContent.classList = "card-content px-0";
+
         var columns = document.createElement("div");
         columns.classList = "columns";
 
-        // var 
+        var columns = document.createElement("div");
+        columns.classList = "columns";
+        
+        var blankDiv = document.createElement("div");
+        blankDiv.innerHTML = "<div class='card-image'><img class='is-rounded ml-6 mr-4' src='./assets/images/aconcert.jpeg'></div>";
 
+        var column = document.createElement("div");
+        column.classList = "column p-0 mt-4 ml-6";
+        var columnClasses = ["title", "title is-4", "subtitle is-6"];
+        var columnContent = ["80s & 90s Rock Rewind", "date", "day + time"];
+        for(var x = 0; x < columnClasses.length; x++){
+            var columnP = document.createElement("p");
+            columnP.classList = columnClasses[x];
+            columnP.textContent = columnContent[x];
+            column.appendChild(columnP);
+        }
+        
+        var ticketBtns = document.createElement("div");
+        ticketBtns.classList = "p-0 mr-3 is-flex is-flex-direction-column is-justify-content-space-between is-align-items-flex-end";
+        var watchlistBtn = document.createElement("button");
+        watchlistBtn.classList = "button mt-4 columns mr-6"
+        watchlistBtn.setAttribute("type", "submit");
+        watchlistBtn.setAttribute("value", "submit input");
+        watchlistBtn.setAttribute("id", "heart-icon");
+        watchlistBtn.textContent = "♡";
 
-        var concertCard = document.createElement("div");
-        concertCard.classList = "column is-narrow";
+        var buyTicketsBtn = document.createElement("button");
+        buyTicketsBtn.classList = "button mr-5 mb-5 is-light is-rounded has-text-centred my-2 p-5";
+        buyTicketsBtn.setAttribute("type", "submit");
+        buyTicketsBtn.setAttribute("id", "submit-button");
+        buyTicketsBtn.textContent = "Buy Tickets";
         
         var cardImg = document.createElement("div");
         cardImg.classList = "card-image";
@@ -171,9 +203,13 @@ function createResults(){
 
         // append DOm elements
         concertList.appendChild(concertCard);
-        concertCard.appendChild(cardImg);
-        cardImg.appendChild(imgBox);
-        cardImg.appendChild(textBox);
+        concertCard.appendChild(cardContent);
+        cardContent.appendChild(columns);
+        columns.appendChild(blankDiv);
+        columns.appendChild(column);
+        columns.appendChild(ticketBtns);
+        ticketBtns.appendChild(watchlistBtn);
+        ticketBtns.appendChild(buyTicketsBtn);
     }
 
 }
