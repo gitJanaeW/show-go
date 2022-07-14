edc49041d86d63d23396f14b38af20ac97c657e6
janae.wel <janae.wel@gmail.com>
2022-07-14 18:19:47 -0400

    notify user of copy

--- copyText.js
+++ copyText.js
@@ -1,12 +1,12 @@
 var copyBox = document.querySelector(".copy-container");
 var copyBtn = document.querySelector(".copy-button");
 
-const copyText = function(event){
+function copyText(event){
     let targetEl = event.target;
     if(targetEl.matches(".copy-button")){
         var targetText = targetEl.previousElementSibling;
         navigator.clipboard.writeText(targetText.textContent);
-        copyBox.innerHTML = "Copied";
+        targetEl.textContent = "Copied";
     }
 }
 
